64504cfe4cca5236d828e4402566f72d80f53a9d
liupcoder <49053793+liupcoder@users.noreply.github.com>
2024-06-14 08:09:04 +0800

    Update replace_supervisor_addon_url.sh

--- replace_supervisor_addon_url.sh
+++ replace_supervisor_addon_url.sh
@@ -5,7 +5,7 @@
 addons_url_file="/usr/src/supervisor/supervisor/const.py"
 
 if [ -n "$(cat ${addons_url_file} | grep -o 'URL_HASSIO_ADDONS = "https://github.com/home-assistant/addons"')" ]; then 
-  sed -i 's/URL_HASSIO_ADDONS = "https\:\/\/github.com\/home-assistant\/addons\"/URL_HASSIO_ADDONS = "https\:\/\/hub.fastgit.xyz\/home-assistant\/addons\"/g' ${addons_url_file}
+  sed -i 's/URL_HASSIO_ADDONS = "https\:\/\/github.com\/home-assistant\/addons\"/URL_HASSIO_ADDONS = "https\:\/\/mirror.ghproxy.com\/home-assistant\/addons\"/g' ${addons_url_file}
   if [ -n "$(cat ${addons_url_file} | grep -o 'URL_HASSIO_ADDONS = "https://mirror.ghproxy.com/home-assistant/addons"')" ]; then
     echo "Succeed! Addons downloading URL was replaced. Supervisor will reboot now to take effect..."
     reboot
